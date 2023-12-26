c845a0f79697fab927cce365de631de71de3a6a1
Brian Lee <bnlee419@gmail.com>
2023-12-25 23:52:10 -0800

    added tailwind, shadcn ui set up finally and added carousel for slide show

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust as needed for your file types
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
